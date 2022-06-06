9ae476557d7f0fbca8d045708d9fc39241bda546
OlekBliter <ggudok054@gmail.com>
2022-06-06 13:42:29 +0300

    change social img on svg, add social in menu

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 $(function(){
     $('.burger').click(function(){
         $('.nav').toggleClass('show-nav')
+        $('.social-box').toggleClass('show-social')
         $('.menu').toggleClass('show-menu')
         $('body').toggleClass('fixed')
     })
